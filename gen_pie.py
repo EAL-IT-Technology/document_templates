1956211836b9eb2f45f00bc46cfe9f5acfedce09
Big Moozer <bigmoozer@gmail.com>
2018-11-07 08:45:54 +0100

    pie generation dcode cleanup

--- gen_pie.py
+++ gen_pie.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 
+# the only values to change
 sizes = [2, 10, 2, 4]
+fig_name = "pie.png"
 
 # Pie chart, where the slices will be ordered and plotted counter-clockwise:
 labels = 'K2', 'K3', 'K4', 'K1'
@@ -15,18 +17,6 @@ label_str = [get_labels( labels[i], sizes[i] ) for i in range(len(labels))]
 print label_str
 
 fig1, ax1 = plt.subplots()
-#wedges, texts, autotexts = ax1.pie(sizes, autopct=lambda : get_labels(size, labels),
-#                                  textprops=dict(color="w"))
-
-#ax1.legend(wedges, ingredients,
-#          title="Ingredients",
-#          loc="center left",
-#          bbox_to_anchor=(1, 0, 0.5, 1))
-
-#plt.setp(autotexts, size=8, weight="bold")
-
-
-#ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
 wedges, texts = ax1.pie(sizes, explode=explode, labels=label_str,
         shadow=False, counterclock=False,
         wedgeprops={"edgecolor":"w",'linewidth': 1, 'linestyle': 'solid', 'antialiased': True},
@@ -37,5 +27,9 @@ ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 for t in texts:
     t.set_horizontalalignment('center')
     t.set_color( "w" )
-plt.show()
 
+# use this to show, if not saving
+#plt.show()
+
+print "image saved {:s}".format( fig_name )
+plt.savefig(fig_name, bbox_inches='tight')
